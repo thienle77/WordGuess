ab5724467bf449c120e39929388ea4b54a3b622f
thienle77 <tle9677@gmail.com>
2022-11-15 21:48:59 -0500

    added my name

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,10 +1,26 @@
 package com.github.zipcodewilmington;
 
 
+import java.util.Scanner;
+
 /**
  * @author xt0fer
  * @version 1.0.0
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
+
+    String words[] = new String[]{ "java", "array", "zipcode", "agile", "software"};
+
+    String randomWord = words[(int) (Math.random()) * words.length()];
+    int lives = 0;
+
+    Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+
 }
